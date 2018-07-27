21702262ed62ac73c410339ed0f5a11991b14a27
Allie321 <allisonroberson04@gmail.com>
2018-07-27 18:38:34 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :author, :page_count, :genre, :brand
+  attr_accessor :author, :page_count, :genre
   attr_reader :title
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @barnd = brand
   end
  
   def turn_page
